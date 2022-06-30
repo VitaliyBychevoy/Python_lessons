62b1a25ce8aff2396edc3c0f488dbc069f54b002
VitaliyBychevoy <bychevoyvitaliy@gmail.com>
2022-06-30 21:46:05 +0300

    30.06.2022 c 372

---
+++ chapter_16/show_color_scales.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+from plotly import colors
+
+for key in colors.PLOTLY_SCALES.keys():
+    print(key)
--- chapter_16/task/task_chapter_16.py
+++ chapter_16/task/task_chapter_16.py
@@ -39,89 +39,65 @@ plt.tick_params(axis='both', which='major', labelsize=16)
 plt.show()
 """
 print("\n\t 16.3 Осадки в Cан Франсиско.")
-
-
 filename_San_Fransisco = "data/San_Fransisco-2022-01-01_2022-06-22.csv"
-
-with open(filename_San_Fransisco) as f:
-    reader = csv.reader(f)
-    header_row = next(reader)
-
-    #for index, column in enumerate(header_row):
-        #print(index, column)
-
-    dates, temp_max, temp_min = [], [], []
-    for row in reader:
-        concurrent_date = datetime.strptime(row[2], "%Y-%m-%d")
-        try:
-            temp = float(row[4])
-            temp_m = float(row[5])
-        except ValueError:
-            print(f"Missing data for {concurrent_date}")
-        else:
-            dates.append(concurrent_date)
-            temp_max.append(temp)
-            temp_min.append(temp_m)
-    f.close()
-
 filename_Sitka = 'data/sitka_weather_2018_simple.csv'
-with open(filename_San_Fransisco) as d:
-    reader_1 = csv.reader(d)
-    header_row = next(reader_1)
-    dates_Sitka, temp_max_Sitka, temp_min_Sitka = [], [], []
-    for row in reader_1:
-        concurrent_date_2 = datetime.strptime(row[2], "%Y-%m-%d")
-        try:
-            temp_1 = float(row[4])
-            temp_2 = float(row[5])
-        except ValueError:
-            print(f"Missing data for {concurrent_date_2}")
-        else:
-            dates_Sitka.append(concurrent_date_2)
-            temp_max_Sitka.append(temp_1)
-            temp_min_Sitka.append(temp_2)
-    d.close()
 filename_dv = 'data/death_valley_2018_simple.csv'
-with open(filename_dv) as h:
-    reader_dv = csv.reader(h)
-    header_row = next(reader_dv)
-    dates_dv, temp_max_dv, temp_min_dv = [], [], []
-    for row in reader_dv:
-        concurrent_date_dv = datetime.strptime(row[2], "%Y-%m-%d")
+
+with open(filename_Sitka) as fS:
+    reader = csv.reader(fS)
+    head_row = next(reader)
+
+    print("Sitka`s column")
+    for i, value in enumerate(head_row):
+        print(i, value)
+
+    dates_for_Sitka, temper_max_for_Sitka, temper_min_for_Sitka  = [], [], []
+    for row in reader:
+        current_date = datetime.strptime(row[2], '%Y-%m-%d')
         try:
-            temp_1_dv = float(row[4])
-            temp_2_dv = float(row[5])
+            temper_max = float(row[5])
+            temper_min = float(row[6])
         except ValueError:
-            print(f"Missing data for {concurrent_date_dv}")
+            print(f"Missing data for {current_date}")
         else:
-            dates_Sitka.append(concurrent_date_dv)
-            temp_max_Sitka.append(temp_1_dv)
-            temp_min_Sitka.append(temp_2_dv)
-    h.close()
-#Plotting Data on a diagram
+            dates_for_Sitka.append(current_date)
+            temper_min_for_Sitka.append(temper_min)
+            temper_max_for_Sitka.append(temper_max)
+fS.close()
+print("\n")
+with open(filename_dv) as fdv:
+    reader_dv = csv.reader(fdv)
+    head_row = next(reader_dv)
+
+    print("DV`s column")
+    for i, val in enumerate(head_row):
+        print(i, val)
+
+    dates_for_dv, temper_max_for_dv, temper_min_for_dv = [], [], []
+    for row in reader_dv:
+        current_date_dv = datetime.strptime(row[2], '%Y-%m-%d')
+        try:
+            temper_max_dv = float(row[4])
+            temper_min_dv = float(row[5])
+        except ValueError:
+            print(f"Missing data for {current_date_dv}")
+        else:
+            dates_for_dv.append(current_date_dv)
+            temper_max_for_dv.append(temper_max_dv)
+            temper_min_for_dv.append(temper_min_dv)
+fdv.close()
+
+
 plt.style.use('seaborn')
-fig, ax = plt.subplots()
-
-#San_Fransisco
-#ax.plot(dates, temp_max, c='red', alpha=0.7)
-#ax.plot(dates, temp_min, c='red')
-#plt.fill_between(dates, temp_max, temp_min, facecolor='red', alpha=0.2)
-
-#Sitka
-ax.plot(dates_Sitka, temp_max_Sitka, c='red')
-ax.plot(dates_Sitka, temp_min_Sitka, c='red')
-#plt.fill_between(dates_Sitka, temp_max_Sitka, temp_min_Sitka, facecolor='green', alpha=0.2)
-
-#DV
-ax.plot(dates_dv, temp_max_dv, c='blue', alpha=0.7)
-ax.plot(dates_dv, temp_min_dv, c='yellow')
-
-
-#Formating diagram
-plt.title("Температура ", fontsize=24)
-plt.xlabel('', fontsize=16)
-fig.autofmt_xdate()
-plt.ylabel('Температура ', fontsize=16)
-plt.tick_params(axis='both', which='major', labelsize=16)
-
+line_Sitka_max = plt.plot(dates_for_Sitka, temper_max_for_Sitka, '-', color='g')
+line_Sitka_min = plt.plot(dates_for_Sitka, temper_min_for_Sitka, '-', color='g')
+line_dv_max = plt.plot(dates_for_dv, temper_max_for_dv, color='r')
+line_dv_min = plt.plot(dates_for_dv, temper_min_for_dv, color='r')
+plt.fill_between(dates_for_Sitka, temper_max_for_Sitka, temper_min_for_Sitka,
+                 color='b', alpha=0.5)
+plt.fill_between(dates_for_dv, temper_max_for_dv,temper_min_for_dv, color='y',
+                 alpha=0.5)
+plt.title("Sitka and Death Valley.")
+plt.ylabel('Temper', fontsize=16)
+plt.xlabel('Time', fontsize=16)
 plt.show()
